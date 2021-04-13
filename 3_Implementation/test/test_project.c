362aa9d49b1491f219c2799b0fb31c3026e635bd
satyadevkalakonda <satyadevkalakonda143@gmail.com>
2021-04-13 16:25:13 +0530

    added total testcases

--- 3_Implementation/test/test_project.c
+++ 3_Implementation/test/test_project.c
@@ -6,6 +6,8 @@
 
 calc_arthmetic value;
 static result ans={0};
+calc_st valuee;
+static result_t anss={0};
 
 void setUp(void) {}
 /* Required by the unity test framework */
@@ -116,12 +118,6 @@ void test_div(void)
     divv(value,&ans);
 	TEST_ASSERT_EQUAL(162,ans.final_answer);
 
-     /**
-     * @brief Scenario based test case for divison 
-     * 
-     */
-    
-   
 
     /**
      * @brief Boundary based test case for divison
@@ -136,6 +132,126 @@ void test_div(void)
 
 }
 
+void test_log(void)
+{
+    /**
+     * @brief requirment based test case.
+     * 
+     */
+     valuee.num_1=8869;
+     logg(valuee,&ans);
+     TEST_ASSERT_EQUAL(3.95,ans.final_answer);
+
+     /**
+      * @brief scenario based test case.
+      * 
+      */
+     valuee.num_1=124;
+     logg(valuee,&ans);
+     TEST_ASSERT_EQUAL(2.09,ans.final_answer);
+
+}
+
+void test_sqrt(void)
+{
+    /**
+     * @brief requirment based
+     * 
+     */
+    valuee.num_1=24;
+    sqrtt(valuee,&ans);
+    TEST_ASSERT_EQUAL(4.90,ans.final_answer);
+    /**
+     * @brief scenario based
+     * 
+     */
+    valuee.num_1=45*32;
+    sqrtt(valuee,&ans);
+    TEST_ASSERT_EQUAL(37.95,ans.final_answer);
+    /**
+     * @brief boundary based
+     * 
+     */
+    valuee.num_1=9999;
+    sqrtt(valuee,&ans);
+    TEST_ASSERT_EQUAL(99,ans.final_answer);
+}
+
+void test_power(void)
+{
+    /**
+     * @brief Requirment based
+     * 
+     */
+    value.num_1=5;
+    value.num_2=20;
+    power(value,&ans);
+    TEST_ASSERT_EQUAL(95367433551872,ans.final_answer);
+
+    /**
+     * @brief Scenario based
+     * 
+     */
+    value.num_1=2*2;
+    value.num_2=4;
+    power(value,&ans);
+    TEST_ASSERT_EQUAL(256,ans.final_answer);
+}
+
+void test_sin(void)
+{
+    /**
+     * @brief requirment based
+     * 
+     */
+    valuee.num_1=10;
+    sinn(valuee,&anss);
+    TEST_ASSERT_EQUAL(99,ans.final_answer);
+    /**
+     * @brief Boundary based
+     * 
+     */
+    valuee.num_1=1000;
+    sinn(valuee,&anss);
+    TEST_ASSERT_EQUAL(99,ans.final_answer);
+}
+void test_cos(void)
+{
+    /**
+     * @brief requirement based
+     * 
+     */
+    valuee.num_1=9;
+    coss(valuee,&anss);
+    TEST_ASSERT_EQUAL(99,ans.final_answer);
+    /**
+     * @brief boundary based
+     * 
+     */
+    valuee.num_1=8888;
+    coss(valuee,&anss);
+    TEST_ASSERT_EQUAL(99,ans.final_answer);
+
+}
+
+void test_tan(void)
+{
+    /**
+     * @brief requirment based
+     * 
+     */
+    valuee.num_1=6;
+    tann(valuee,&anss);
+    TEST_ASSERT_EQUAL(99,ans.final_answer);
+    /**
+     * @brief Boundary based
+     * 
+     */
+    valuee.num_1=5555;
+    coss(valuee,&anss);
+    TEST_ASSERT_EQUAL(99,ans.final_answer);
+
+}
 
 int main()
 {
@@ -148,5 +264,10 @@ int main()
     RUN_TEST(test_sub);
     RUN_TEST(test_mul);
     RUN_TEST(test_div);
+    RUN_TEST(test_log);
+    RUN_TEST(test_sqrt);
+    RUN_TEST(test_sin);
+    RUN_TEST(test_cos);
+    RUN_TEST(test_tan);
 	return UNITY_END();
 }
